8d486537ff1081e6d4cd05204054bede9bece1cb
Ilyosjon <flutter.developer.uzb@gmail.com>
2026-05-17 15:14:05 +0500

    first bot

---
+++ config.py
@@ -0,0 +1,14 @@
+TOKEN = "8839269146:AAHRwLqxqRjNQQJb3erttdJ0aT5A1mrlkCo"
+
+CHANNEL = "@mahorat_maktabi_namangan"
+
+ADMIN_IDS = [
+    308239496,
+    1557783008
+]
+
+CARD_NUMBER = "9860 0401 1490 7971"
+
+PAYMENT = "50 000 so'm"
+
+SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz7UosUoXO7EL6wVCKZ3fa2WlhFKs-joBLLSpQQawp92tyJZuk4Oy2Oe9yAi0DQrYzw/exec"
